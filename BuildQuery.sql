9d9d51e2c26c1055351c70d78336b24340cad3e9
DominicHudson102983679 <61397178+DominicHudson102983679@users.noreply.github.com>
2020-08-10 09:37:51 +1000

    Task 2 Complete

--- BuildQuery.sql
+++ BuildQuery.sql
@@ -1,7 +1,5 @@
 /* Dominic Hudson - 102983679 */
 
-use database BuildQuery;
-
 /* 
 CLIENT (ClientID, Surname, GivenName, Gender)
     PK (ClientID)
@@ -19,3 +17,63 @@ BOOKING (DateBooked, Payment, EventYear, EventMonth, EventDay, TourName, ClientI
     FK (TourName) references TOUR
     FK (ClientID) references CLIENT
 */
+
+/*
+use BuildQuery;
+*/
+
+/* ------------------------------------------------------ Task 1
+
+CREATE TABLE TOUR (
+    TourName NVARCHAR(100),
+    Description NVARCHAR(500),
+    PRIMARY KEY (TourName)
+);
+
+CREATE TABLE CLIENT (
+    ClientID INT,
+    Surname NVARCHAR(100) NOT NULL,
+    GivenName NVARCHAR(100) NOT NULL,
+    Gender NVARCHAR(1) CHECK (Gender IN ('M', 'F', 'I')),
+    PRIMARY KEY (ClientId)
+)
+
+CREATE TABLE EVENT (
+    TourName NVARCHAR(100),
+    EventMonth NVARCHAR(3) CHECK (EventMonth IN ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec')),
+    EventDay INT CHECK (EventDay >= 1 AND EventDay <= 31),
+    EventYear INT CHECK ((EventYear) >= 4),
+    EventFee Money NOT NULL CHECK ((EventFee) >= 0),
+    PRIMARY KEY (EventMonth, EventDay, EventYear),
+    FOREIGN KEY (TourName) REFERENCES TOUR
+)
+
+CREATE TABLE BOOKING ( 
+    EventMonth NVARCHAR(3) CHECK (EventMonth IN ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec')),
+    EventDay INT CHECK (EventDay >= 1 AND EventDay <= 31),
+    EventYear INT CHECK ((EventYear) >= 4),
+    ClientID INT,
+    TourName NVARCHAR(100),
+    Payment MONEY CHECK ((Payment) > 0),
+    DateBooked DATE NOT NULL,
+    PRIMARY KEY (ClientID, TourName, EventMonth, EventDay, EventYear),
+    FOREIGN KEY (TourName) REFERENCES TOUR,
+    FOREIGN KEY (ClientID) REFERENCES CLIENT,
+    FOREIGN KEY (EventMonth, EventDay, EventYear) REFERENCES EVENT
+)
+
+*/
+
+/* ----------------------------------------------- Task 2
+*/
+SELECT TourName, Description
+FROM Tour;
+
+SELECT ClientId, SurName, GivenName, Gender
+FROM Client;
+
+SELECT TourName, EventMonth, EventDay, EventYear, EventFee
+FROM Event;
+
+SELECT EventMonth, EventDay, EventYear, ClientID, TourName, Payment, DateBooked
+FROM Booking;
